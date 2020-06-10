3da5584afe31ac57536fa056f2c76471a4419b54
Daniele <danielefontana90@gmail.com>
2020-06-10 20:50:57 +0200

    fix box shadow,hover

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+
 $(document).ready(
   function() {
-  $(".menu-dropdown").mouseenter(
-        function() {
-        $(this).children(".dropdown").addClass("active");            }
-    );
+    $(".menu-dropdown").mouseenter(
+      function() {
+        // $(".menu-dropdown.dropdown").removeClass('active');
+        $(this).children(".dropdown").addClass("active");
+
+      }
+    )
     $(".menu-dropdown").click(
       function() {
         $(this).children(".dropdown").toggleClass("active");
       }
     )
   }
-);
+)
